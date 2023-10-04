d37f21edc46611f0a601db25a9cd8359f6394fd0
LucasPmep <lucas.gillet@pme-partner.fr>
2023-10-04 12:49:22 +0200

    :recycle: refactor with component

---
+++ src/components/CardPokemon.jsx
@@ -0,0 +1,74 @@
+export default function PokemonCard ({pokemon}) {
+    return ( 
+        <article style={{borderColor: getColorHexa(pokemon.apiTypes[0].name), backgroundColor: getColorHexa(pokemon.apiTypes[0].name)}}> 
+            <figure>
+                <picture>
+                    <img src={pokemon.image} alt={"Image " + pokemon.name} />
+                </picture>
+                <figcaption>
+                    <span className="types">{pokemon.apiTypes[0].name}</span>
+                    <h2>{pokemon.name}</h2>
+                    <ol>
+                        <li>Points de vie : {pokemon.stats.HP}</li>
+                        <li>Attaque : {pokemon.stats.attack}</li>
+                        <li>Défense : {pokemon.stats.defense}</li>
+                        <li>Attaque spécial : {pokemon.stats.special_attack}</li>
+                        <li>Vitesse : {pokemon.stats.speed}</li>
+                    </ol>
+                </figcaption>
+            </figure>
+        </article>
+    )
+}
+
+function getColorHexa (typeName) {
+    switch (typeName) {
+        case 'Eau':
+            return 'blue';
+
+        case 'Plante':
+            return 'green';
+
+        case 'Poison':
+            return '#D850C2';
+
+        case 'Vol':
+            return '#738DDB';
+
+        case 'Feu':
+            return 'orange';
+
+        case 'Insecte':
+            return '#70B901';
+
+        case 'Électrik':
+            return '#FFD244';
+
+        case 'Sol':
+            return '#CD793F';
+
+        case 'Fée':
+            return 'pink';
+
+        case 'Combat':
+            return 'darkred';
+
+        case 'Psy':
+            return '#FD6960';
+
+        case 'Acier':
+            return '#246A79';
+
+        case 'Glace':
+            return '#67D1C8';
+
+        case 'Roche':
+            return '#CBB866';
+
+        case 'Dragon':
+            return '#1C6ABB';
+
+        default:
+            return 'grey';
+    }
+}
